9007fa1c10e31f4d55d48995f1b2b10fea43bc9f
Francesco Scarniglia <francescoscarniglia@gmail.com>
2020-06-28 14:04:22 +0200

    layout admin

---
+++ laravel-auth/resources/views/admin/posts/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="container">
+    <h1 class="mb-4">Blog archive</h1>
+    @foreach ($posts as $post)
+        <article>
+        <h2> {{ $post->title }} </h2>
+        <p> {{ $post->body }} </p>
+        @if ( !$loop->last )
+            <hr>
+        @endif
+        </article>
+    @endforeach
+
+    <div class="d-flex justify-content-end">
+        {{ $posts->links() }}
+    </div>
+
+
+</div>
+@endsection
