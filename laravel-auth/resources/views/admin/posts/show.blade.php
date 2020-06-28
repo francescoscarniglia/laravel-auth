38df1429c444edce63a53bee4e22b502cfab9fc7
Francesco Scarniglia <francescoscarniglia@gmail.com>
2020-06-28 17:06:10 +0200

    add show
    add controller 
    add view
    add link

--- laravel-auth/resources/views/admin/posts/show.blade.php
+++ laravel-auth/resources/views/admin/posts/show.blade.php
@@ -2,28 +2,26 @@
 
 @section('content')
 <div class="container">
-    <h1 class="mb-4">Blog archive</h1>
+    <h1 class="mb-4">{{ $post->title }}</h1>
 
     <table class="table">
         <thead>
             <tr>
                 <th>ID</th>
                 <th>Title</th>
+                <th>Body</th>
                 <th>Created at</th>
                 <th>Last update</th>
                 <th colspan="3"></th>
             </tr>
         </thead>
         <tbody>
-            @foreach ($posts as $post)
                 <tr>
                     <td>{{ $post->id }}</td>
                     <td>{{ $post->title }}</td>
+                    <td>{{ $post->body }}</td>
                     <td>{{ $post->created_at }}</td>
                     <td>{{ $post->update_at }}</td>
-                    <td>
-                    <a class="btn btn-success" href="{{ route('admin.posts.show', $post->id) }}"> Show </a>
-                    </td>
 
                     <td>
                         <a class="btn btn-info" href=""> Edit </a>
@@ -34,15 +32,9 @@
                     </td>
 
                 </tr>
-            @endforeach
+
         </tbody>
     </table>
-
-
-    <div class="d-flex justify-content-end">
-        {{ $posts->links() }}
-    </div>
-
 
 </div>
 @endsection
