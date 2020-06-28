e26442154ee9ec6a3da96b2bf0b6d4bbea78a087
Francesco Scarniglia <francescoscarniglia@gmail.com>
2020-06-28 12:56:59 +0200

    continue ex.
    add postcontroller 
    resolve collision in name (routes)

--- laravel-auth/routes/web.php
+++ laravel-auth/routes/web.php
@@ -24,10 +24,13 @@ Route::get('posts', 'PostController@index')->name('post.index');
 
 // ADMIN
 Route::prefix('admin')
+    ->name('admin.')
      ->namespace('Admin')
      ->middleware('auth')
      ->group(function(){
-        Route::get('/home', 'HomeController@index')->name('admin.home');
+        Route::get('/home', 'HomeController@index')->name('home');
+
+        Route::resource('posts', 'PostController');
      });
 
 
